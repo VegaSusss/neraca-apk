ee4aaa8fedad794d55c6a53f0cb1388c1b08bc0c
VegaSusss <derryuya@gmail.com>
2025-06-16 15:27:07 +0700

    Update JurnalTransaksiActivity.java

--- app/src/main/java/com/example/user/neraca_1/JurnalTransaksiActivity.java
+++ app/src/main/java/com/example/user/neraca_1/JurnalTransaksiActivity.java
@@ -2,79 +2,53 @@ package com.example.user.neraca_1;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity; // pakai AppCompatActivity modern
+
 import com.example.user.test.R;
 
-public class JurnalTransaksiActivity extends ActionBarActivity {
+public class JurnalTransaksiActivity extends AppCompatActivity {
 
     String[] barang = {"Uang", "Mie Kuning", "Daging Sapi", "Kerupuk", "Bakso"};
     String[] jumlah = {"Rp100.000", "1 kg", "1 kg", "15 buah", "Habis"};
+
     TextView nama;
     TextView nominal;
-    int i;
+    int i = 0;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.jurnal_transaksi);
 
-        nama = (TextView) findViewById(R.id.nama_barang);
-        nominal = (TextView) findViewById(R.id.nominal_barang);
+        nama = findViewById(R.id.nama_barang);
+        nominal = findViewById(R.id.nominal_barang);
 
-        i = 0;
-        nama.setText(barang[i]);
-        nominal.setText(jumlah[i]);
-
-        /*TextView test = (TextView)findViewById(R.id.textView11);
-        mydb = new DBHelper(this);
-        ArrayList<String> AL = mydb.getAllTransaksi();
-
-        int row_num = Integer.parseInt(AL.get(0));
-
-        String result = "";
-        for (int i = 0; i < row_num ; i++){
-            result += AL.get(i*4+1) + "|" + AL.get(i*4+2) + "|" + AL.get(i*4+3) + "|" + AL.get(i*4+4) + "\n";
-        }
-
-        result += "\n\n";
-
-        ArrayList<String> ALOR = mydb.getAllObjekRef();
-
-        int row_num_or = Integer.parseInt(ALOR.get(0));
-
-        for (int j = 0; j < row_num_or ; j++){
-            result += ALOR.get(j*4+1) + "|" + ALOR.get(j*4+2) + "|" + ALOR.get(j*4+3) + "|" + ALOR.get(j*4+4) + "\n";
-        }
-
-        test.setText(result);*/
+        updateData();
     }
 
     public void GeserKanan(View view) {
-        i++;
-        Geser();
+        i = (i + 1) % barang.length;
+        updateData();
     }
 
     public void GeserKiri(View view) {
-        i--;
-        Geser();
+        i = (i - 1 + barang.length) % barang.length;
+        updateData();
     }
 
-    public void Geser() {
-        if (i < 0)
-            i += 5;
+    private void updateData() {
+        nama.setText(barang[i]);
+        nominal.setText(jumlah[i]);
 
-        nama.setText(barang[i % 5]);
-        nominal.setText(jumlah[i % 5]);
-
-        if (jumlah[i % 5].equals("Habis")) {
+        if ("Habis".equals(jumlah[i])) {
             nominal.setTextColor(Color.BLACK);
         } else {
-            nominal.setTextColor(Color.argb(255,11,159,38));
+            nominal.setTextColor(Color.argb(255, 11, 159, 38));
         }
     }
-
 }
